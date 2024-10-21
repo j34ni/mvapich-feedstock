e6ed29ea6c79aff758f2fb38a4709a66b8fdb6a7
j34ni <jeani@uio.no>
2024-10-21 08:43:47 +0000

    Added support for linux-aarch64 and linux-ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,23 @@ export FC=$(basename "$FC")
 
 build_with_rdma=""
 if [[ "$target_platform" == linux-* ]]; then
-    echo "Build with RDMA support"
-    build_with_rdma="--with-rdma=$PREFIX --enable-rdma-cm "
+  echo "Build with RDMA support"
+  build_with_rdma="--with-rdma=$PREFIX --enable-rdma-cm "
+fi
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "linux-ppc64le" ]]; then
+    export CROSS_F77_SIZEOF_INTEGER=4
+    export CROSS_F77_SIZEOF_REAL=4
+    export CROSS_F77_SIZEOF_DOUBLE_PRECISION=8
+    export CROSS_F77_TRUE_VALUE=1
+    export CROSS_F77_FALSE_VALUE=0
+    export CROSS_F90_ADDRESS_KIND=8
+    export CROSS_F90_OFFSET_KIND=8
+    export CROSS_F90_INTEGER_KIND=4
+    export CROSS_F90_REAL_MODEL=" 6 , 37"
+    export CROSS_F90_DOUBLE_MODEL=" 15 , 307"
+  fi
 fi
 
 ./configure --prefix=$PREFIX \
