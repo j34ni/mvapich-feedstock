f46bd073d39dd61af6a45d34353264a2e3bbfcfe
Jean Iaquinta <jeani@freebio4.hpc.uio.no>
2025-10-16 19:57:02 +0200

    Add CONDA_BUILD_CROSS_COMPILATION

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,21 @@ export CC=$(basename "$CC")
 export CXX=$(basename "$CXX")
 export FC=$(basename "$FC")
 
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  if [[ "$target_platform" == "linux-aarch64" ]]; then
+    export CROSS_F77_SIZEOF_INTEGER=4
+    export CROSS_F77_SIZEOF_REAL=4
+    export CROSS_F77_SIZEOF_DOUBLE_PRECISION=8
+    export CROSS_F77_TRUE_VALUE=1
+    export CROSS_F77_FALSE_VALUE=0
+    export CROSS_F90_ADDRESS_KIND=8
+    export CROSS_F90_OFFSET_KIND=8
+    export CROSS_F90_INTEGER_KIND=4
+    export CROSS_F90_REAL_MODEL=" 6 , 37"
+    export CROSS_F90_DOUBLE_MODEL=" 15 , 307"
+  fi
+fi
+
 export CUDA_HOME="${PREFIX}/targets/x86_64-linux"
 if [[ "$target_platform" == "linux-aarch64" ]]; then
   export CUDA_HOME="${PREFIX}/targets/sbsa-linux"
